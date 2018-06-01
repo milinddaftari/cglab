ebe052d5ae7e057f99b9e6c825d807de302b7dbb
Milind Daftari <milind007.md@gmail.com>
2018-06-01 19:48:42 +0530

    NEW CODE - Works for all slopes

--- brezenham.c
+++ brezenham.c
@@ -1,125 +1,107 @@
-#include<stdio.h> 
-#include<stdlib.h>
+#include<stdio.h>
 #include<GLUT/glut.h>
 
-int x1,y1,x2,y2;
-
-void drawpixel(int x,int y)
+int x1, y1, x2, y2;
+void draw_pixel(int x, int y)
 {
-	glColor3f(1.0,0.0,0.0);
+	glColor3f(1, 0, 0);
 	glBegin(GL_POINTS);
-		glVertex2i(x,y);
+	glVertex2i(x, y);
 	glEnd();
 }
 
-void bresenham(int x1,int y1,int x2,int y2)
+void draw_line2(int x1, int y1, int x2, int y2)
 {
-	int dx=x2-x1;
-	int dy=y2-y1;
-	int m=dy/dx;
+	int dx, dy, i, d_p;
+	int incx, incy, inc0, inc1;
+	int x, y;
+	
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if(dx < 0) 
+		dx = -dx;
+	if(dy < 0) 
+		dy = -dy;
 
-	if(m<1)
+	if(x2 < x1)
+		incx = -1;
+	else
+		incx = 1;
+	if(y2 < y1)
+		incy = -1;
+	else
+		incy = 1;
+
+	x = x1;
+	y = y1;
+	
+	if(dx > dy)			//gentle slope
 	{
-		int dp=2*dy-dx;
-		int x=x1;
-		int y=y1;
-		if(dx<0)
+		draw_pixel(x, y);
+		d_p = 2 * dy - dx;
+		inc0 = 2 * dy - 2 * dx;
+		inc1 = 2 * dy;
+		while(x != x2)
 		{
-			x=x2;
-			y=y2;
-			x2=x1;
-		}
-	drawpixel(x,y);
-	while(x<x2)
-	{
-		if(dp>=0)
-		{
-			x++;
-			y++;
-			dp=dp+2*dy-2*dx*(y+1-y);
-		}
-		else
-		{
-			x++;
-			y=y;
-			dp=dp+2*dy-2*dx*(y-y);
-		}
-		drawpixel(x,y);
-	}
-	}
-	else if(m>1)
-	{
-		int dp=2*dx-dy;
-		int x=x1;
-		int y=y1;
-		if(dy<0)
-		{
-			x=x2;
-			y=y2;
-			y2=y1;
-		}
-		drawpixel(x,y);
-		while(y<y2)
-		{
-			if(dp>=0)
+			if(d_p>=0)
 			{
-				x++;
-				y++;
-				dp=dp+2*dx-2*dy*(x+1-x);
+				y += incy;
+				d_p += inc0;
 			}
 			else
-			{
-				y++;
-				x=x;
-				dp=dp+2*dx-2*dy*(x-x);
-			}
-			drawpixel(x,y);
-		}
+				d_p += inc1;
+			x += incx;
+			draw_pixel(x, y);
+		}	
 	}
-	else if(m==1)
+	else				//steep slope
 	{
-		int x=x1;
-		int y=y1;
-		drawpixel(x,y);
-		while(x<x2)
+		draw_pixel(x, y);
+		d_p = 2 * dx - dy;
+		inc0 = 2 * dx - 2 * dy;
+		inc1 = 2 * dx;
+		while(y != y2)
 		{
-			x=x+1;
-			y=y+1;
-			drawpixel(x,y);
+			if(d_p>=0)
+			{
+				x += incx;
+				d_p += inc0;
+			}
+			else
+				d_p += inc1;
+			y += incy;
+			draw_pixel(x, y);
 		}
 	}
 }
-
 void myinit()
 {
-	glClearColor(1.0,1.0,1.0,1.0);
+	glClearColor(1,1,1,1);
 	glPointSize(5.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0,500,0,500);
+	gluOrtho2D(0, 500, 0, 500);
 	glMatrixMode(GL_MODELVIEW);
 }
-
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	bresenham(x1,y1,x2,y2);
+	draw_line2(x1, y1, x2, y2);
 	glFlush();
 }
 
-int main(int argc,char** argv)
+void main(int argc, char ** argv)
 {
-	printf("Enter Scan Points (x1,y1) ");
-	scanf("%d %d",&x1,&y1);
-	printf("Enter Scan Points (x2,y2) ");
-	scanf("%d %d",&x2,&y2);
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(50,50);
-	glutCreateWindow("Brezenham");
-	glutDisplayFunc(display);
+	printf("Enter x1,y1\n");
+	scanf("%d %d",&x1, &y1);
+	printf("Enter x2,y2\n");
+	scanf("%d %d",&x2, &y2);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Bressenham");
 	myinit();
+	glutDisplayFunc(display);
 	glutMainLoop();
 }
-
